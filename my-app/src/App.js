e2846e2e349385eb6db4b273f01d7631315e1b52
Emily Fang <fangemily60@gmail.com>
2020-01-07 15:35:59 -0500

    Decrement + CSS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,10 +9,49 @@ class ContactCard extends Component {
 		<h1>Name: {this.props.name}</h1>
 		<h1>Phone Number: {this.props.phone}</h1>
 		<h1>Email Address: {this.props.email}</h1>
-	    </div>
+		</div>
 	);
     }
 };	      
+
+class Count extends Component{
+    constructor(props) {
+	super(props);
+	this.state = {
+	    current: props.current
+	}
+	this.Increment = this.Increment.bind(this);	
+	this.Decrement = this.Decrement.bind(this);
+    }
+    
+    Increment = () => {
+	let num = this.state.current + 1;
+	this.setState({current: num});
+	console.log(this.state.current);
+    }
+
+    Decrement = () => {
+	let num = this.state.current - 1;
+	if(num >= 0){	
+	    this.setState({current: num});
+	    console.log(this.state.current);
+	}
+	else{
+	    alert("Stop! You can't decrement anymore.");
+	}
+    }
+    render(){
+	return (
+		<div id='count'>
+		<button id='increase' type='button' onClick={this.Increment.bind(this)}>Increment</button>
+		<button id='decrease' type='button' onClick={this.Decrement.bind(this)}>Decrement</button> 
+		<p>Count: {this.state.current}</p>
+		</div>
+	);
+	
+    }
+}
+
 
 function App() {
     return (
@@ -20,6 +59,7 @@ function App() {
 	    <ContactCard name="Emily" phone={45822873} email="stuff@stuff.edu"> </ContactCard>
 	    <ContactCard name="Brian" phone={12345678} email="morestuff@stuff.edu"> </ContactCard>
 	    <ContactCard name="Jessica" phone={873547632} email="moststuff@stuff.edu"> </ContactCard>
+	    <Count current={1}/>
 	    </div>
     );
 }
